2a92c48791d356f53fcb5918b98d6ac6c9a10591
Ubuntu <ubuntu@ip-172-31-34-163.ap-northeast-2.compute.internal>
2020-12-20 15:01:07 +0000

    minor upd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Music({id, name, rating, image}) {
+    return (
+        <div>
+            <center>
+                <h3>
+                    {id}번째 Album : {name} <br/>
+                    rating : {rating} / 10.0
+                </h3>
+                <img src={image} alt={name} />
+            </center>
+        </div>
+    );
+}
+
+const covers = [
+    {
+        id: 1,
+        name: "Future Nostalgia",
+        image: "/images/1.jpg",
+        rating: 7.6
+    },
+    {
+        id: 2,
+        name: "The New Abnormal",
+        image: "/images/2.jpg",
+        rating: 8.4
+    },
+    {
+        id: 3,
+        name: "Punisher",
+        image: "/images/3.jpg",
+        rating: 7.0
+    }
+];
+
+Music.propTypes = {
+    id : PropTypes.number.isRequired,
+    name : PropTypes.string.isRequired,
+    rating : PropTypes.number.isRequired,
+    image : PropTypes.string.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+            <center>대중 음악의 이해</center>
+        </h1>
+        <h2>
+            <center>정기 음평회</center>
+        </h2>
+        {covers.map(cover => 
+            <Music
+                key={cover.id} 
+                id={cover.id}
+                name={cover.name}
+                rating={cover.rating}
+                image={cover.image}
+            />
+        )}
     </div>
   );
 }
